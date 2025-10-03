7f896f361c28234565afbc783e3d7faf1f3125f1
Rexi666 <138510729+Rexi666@users.noreply.github.com>
2025-10-03 19:33:20 +0200

    Update chatbot.js

--- docs/chatbot/chatbot.js
+++ docs/chatbot/chatbot.js
@@ -4,7 +4,7 @@ const API_KEY = "AIzaSyAtAyvf4qdvWIMmpVg6tnyTq2Cs0zeyYG8";
 
 // Ahora sí puedes inicializar Gemini de la forma correcta
 const genAI = new GoogleGenerativeAI(API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 // DOM
 const messages = document.getElementById('messages');
@@ -104,3 +104,4 @@ closeChatBtn.addEventListener('click', () => {
   chatbox.style.display = 'none';
   chatToggleBtn.style.display = 'flex';
 });
+
